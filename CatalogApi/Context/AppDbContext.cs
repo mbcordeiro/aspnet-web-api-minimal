f5519c2a807c0323f2ff2e1515a28038fd6cd47b
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2024-01-16 23:43:23 -0300

    Add db context

---
+++ CatalogApi/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CatalogApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
