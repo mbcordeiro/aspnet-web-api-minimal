3671710ae424371ea0fe2f0992cff173a44d4fb8
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2024-01-17 01:27:19 -0300

    Add endpoints products

--- CatalogApi/Program.cs
+++ CatalogApi/Program.cs
@@ -48,6 +48,47 @@ app.MapDelete("/categories/{id:int}", async (int id, AppDbContext db) =>
     return Results.NoContent();
 });
 
+app.MapPost("/products", async (Product product, AppDbContext db)
+ => {
+     db.Products.Add(product);
+     await db.SaveChangesAsync();
+     return Results.Created($"/products/{product.ProductId}", product);
+ });
+
+app.MapGet("/products", async (AppDbContext db) => await db.Products.ToListAsync());
+
+app.MapGet("/products/{id:int}", async (int id, AppDbContext db)
+    => {
+        return await db.Products.FindAsync(id) 
+        is Product product 
+        ? Results.Ok(product): Results.NotFound();
+    });
+
+app.MapPut("/products/{id:int}", async (int id, Product product, AppDbContext db) =>
+{
+    if (product.ProductId != id) return Results.BadRequest();
+    var existProduct = await db.Products.FindAsync(id);
+    if (existProduct is null) return Results.NotFound();
+    existProduct.Name = product.Name;
+    existProduct.Description = product.Description;
+    existProduct.Price = product.Price;
+    existProduct.ImageUrl = product.ImageUrl;
+    existProduct.RegistrationDate = product.RegistrationDate;
+    existProduct.Stock = product.Stock;
+    existProduct.CategoryId = product.CategoryId;
+    await db.SaveChangesAsync();
+    return Results.Ok(existProduct);
+});
+
+app.MapDelete("/products/{id:int}", async (int id, AppDbContext db) =>
+{
+    var products = await db.Products.FindAsync(id);
+    if (products is null) return Results.NotFound();
+    db.Products.Remove(products);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
