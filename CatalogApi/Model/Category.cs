252d0f249609cf2710708bf44c8b3e004b98e753
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2024-01-16 23:41:46 -0300

    Add models class

---
+++ CatalogApi/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CatalogApi.Model
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public ICollection<Product>? Products { get; set; }
+    }
+}
