a38b043a5aef5372824d0231356ac124af8b316b
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2024-01-17 01:21:50 -0300

    Add put and post category

--- CatalogApi/Program.cs
+++ CatalogApi/Program.cs
@@ -21,11 +21,31 @@ app.MapPost("/categories", async (Category categroy, AppDbContext db) =>
 
 app.MapGet("/categories", async (AppDbContext db) => await db.Categories.ToListAsync());
 
-app.MapGet("/categories/{id}", async (int id, AppDbContext db) =>
+app.MapGet("/categories/{id:int}", async (int id, AppDbContext db) =>
 {
     return await db.Categories.FindAsync(id) 
     is Category category 
     ? Results.Ok() : Results.NotFound();
+});
+
+app.MapPut("/categories/{id:int}", async (int id, Category category, AppDbContext db) =>
+{
+    if (category.CategoryId != id) return Results.BadRequest();
+    var categoryExist = await db.Categories.FindAsync(id);
+    if (categoryExist is null) return Results.NotFound();
+    categoryExist.Name = category.Name;
+    categoryExist.Name = category.Description;
+    await db.SaveChangesAsync();
+    return Results.Ok(categoryExist);
+});
+
+app.MapDelete("/categories/{id:int}", async (int id, AppDbContext db) =>
+{
+    var category = await db.Categories.FindAsync(id);
+    if (category is null) return Results.NotFound();
+    db.Categories.Remove(category);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
 
 if (app.Environment.IsDevelopment())
